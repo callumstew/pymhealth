b25a746a3e23c2aad04cfdbd1c00891ef90f5deb
Callum Stewart <callum.stewart@kcl.ac.uk>
2019-12-11 23:12:00 +0000

    PPG physionet pulse detection docs

--- src/mhealth/heart/ppg.py
+++ src/mhealth/heart/ppg.py
@@ -17,11 +17,17 @@ def incremental_merge_segmentation(x, m=64):
         pass
 
 
-def pulse_onset_zong(x, sampling_rate):
-    """ Pulse onset detection based on (Zong et al 2003) WIP
-    10.1109/CIC.2003.1291140
+def pulse_onset_physionet(ppg: np.ndarray, sampling_rate: int):
+    """Pulse onset detection based on the physionet PPG algorithm.
+
+    (Zong et al 2003). 10.1109/CIC.2003.1291140
+    Params:
+        ppg (np.ndarray[float]): PPG signal
+        sampling_rate (int): Sampling rate of the signal
+    Returns:
+        np.ndarray[int]: Array of detected pulse indices.
     """
-    x = butterworth(x, [0.5, 20], sampling_rate, ftype='bandpass')
+    x = butterworth(ppg, (0.5, 20), sampling_rate, ftype='bandpass')
     w = int(sampling_rate / (1000 / 150))
     x = slope_sum(x, w)
     onsets = physionet_decision_rule(x, sampling_rate)
@@ -29,7 +35,15 @@ def pulse_onset_zong(x, sampling_rate):
 
 
 @jit
-def slope_sum(x, w):
+def slope_sum(x: np.ndarray, w: int):
+    """Sum of the dirivitive of a sliding windowed input signal.
+
+    Params:
+        x (np.ndarray): Input signal
+        w (int): Window size
+    Returns:
+        np.ndarray: Sum of the slope of the signal
+    """
     out = np.zeros(len(x))
     dx = np.diff(x)
     for i in range(w, len(x)-1):
@@ -38,7 +52,17 @@ def slope_sum(x, w):
 
 
 @jit
-def physionet_decision_rule(x, sampling_rate):
+def physionet_decision_rule(x: np.ndarray, sampling_rate: int):
+    """Pulse onset decision rule based on the physionet PPG algorithm.
+
+    (Zong et al 2003). 10.1109/CIC.2003.1291140
+
+    Params:
+        x (np.ndarray[float]): Filtered PPG signal
+        sampling_rate (int): Sampling rate of the PPG signal
+    Returns:
+        np.ndarray[int]: An array of pulse onset indices.
+    """
     th = 2 * np.std(x[:sampling_rate * 10]) + np.mean(x[:sampling_rate * 10])
     th_sub_std = th - np.std(x[:sampling_rate * 10])
     w150 = int(sampling_rate / (1000 / 150))
